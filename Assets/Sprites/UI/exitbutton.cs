dc6c939db9582801e620711f222a58d909d93e42
stevepennings <stevepennings@live.nl>
2017-04-05 12:57:03 +0100

    Menu, particles, photoshop sprites, button koppeling met multiplayer lobby

---
+++ Assets/Sprites/UI/exitbutton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exitbutton : MonoBehaviour {
+
+	public void exitButtonClicked()
+	{
+		Application.Quit();
+		Debug.Log ("Exit Button Clicked");
+	}
+}
---
+++ Assets/sceneManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour {
+	public GameObject LobbyManager;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void multiplayerOnclick(){
+		SceneManager.LoadScene(1);
+		LobbyManager.SetActive (true);
+	}
+
+	public void menuOnclick(){
+		SceneManager.LoadScene(0);
+	}
+}
